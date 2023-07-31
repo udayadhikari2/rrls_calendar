1a56f3c26d0dc6ea563a88af1ab69c3ab1462612
Uday Adhikari <uday.adhikari2@gmail.com>
2023-07-31 22:27:20 +0545

    search and buttons

--- components/home/findingMonths/FindingMonths.jsx
+++ components/home/findingMonths/FindingMonths.jsx
@@ -1,9 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import styles from "./FindingMonths.style";
+import { COLORS, SIZES } from "../../../constants/index"
+import { Ionicons } from "@expo/vector-icons"
 const FindingMonths = () => {
+
+  const SearchData = () => {
+
+  }
+
   return (
     <View style={styles.monthsContainer}>
-      <Text style={styles.monthsTitle}>Find your desired {'\n'} Months?</Text>
+      <Text style={styles.monthsHeading}>Find your desired</Text>
+      <Text style={styles.MonthsTextStyle}>Months?</Text>
+      <View style={styles.searchInputContainer}>
+        <TextInput style={styles.searchInput} placeholder='Search the Month looking for.....' onChange={SearchData} placeholderTextColor={COLORS.gray2} editable color={COLORS.white} size={SIZES.xsMedium} />
+        <TouchableOpacity style={styles.searchButton} disabled>
+          <Ionicons name="search-circle" size={42} color={COLORS.white}></Ionicons>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.nextPrevContainer}>
+        <TouchableOpacity style={styles.prevButton}>
+          <Ionicons name="arrow-back-circle" size={35} color={COLORS.red} />
+        </TouchableOpacity >
+        <View style={styles.monthTitle}>
+          <Text style={styles.monthText}>Baishak</Text>
+        </View>
+        <TouchableOpacity style={styles.nextButton}>
+          <Ionicons name="arrow-forward-circle" size={35} color={COLORS.red} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- components/home/findingMonths/FindingMonths.style.js
+++ components/home/findingMonths/FindingMonths.style.js
@@ -6,11 +6,62 @@ const styles = StyleSheet.create({
         flex: 1,
         marginHorizontal:15,
     },
-    monthsTitle: {
-       
+    monthsHeading: {
         fontFamily: "extraBold",
         fontSize: SIZES.large +12,
         color: COLORS.primary,
+        
+    },
+    MonthsTextStyle:{
+        fontFamily: "extraBold",
+        fontSize: SIZES.large +12,
+        color: COLORS.tertiary,
+        lineHeight:35,
+        paddingBottom:5,
+    },
+    searchInputContainer:{
+        flexDirection:"row",
+        justifyContent:"center",
+    },
+    searchInput:{
+        width:'100%',
+        borderWidth:1,
+        borderRadius:10,
+        backgroundColor:COLORS.primary ,
+        paddingHorizontal:15,
+        paddingVertical:10,
+        borderColor:COLORS.lightWhite,
+    },
+    searchButton:{
+        position:"absolute",
+        right:0,
+        alignItems:"center",
+    },
+    nextPrevContainer:{
+        flexDirection:"row",
+        justifyContent:"center",
+        marginVertical:10,
+        padding:5,
+    },
+    prevButton:{
+        paddingHorizontal:30,
+    },
+    monthTitle:{
+        paddingHorizontal:30,
+        paddingVertical:2,
+        backgroundColor:COLORS.gray1,
+        borderWidth:1,
+        borderRadius:20,
+        borderColor:COLORS.lightWhite,
+        justifyContent:"center",
+    },
+    monthText:{
+        color:COLORS.primary,
+        fontFamily:"bold",
+        fontSize:SIZES.large,
+    },
+    nextButton:{
+        paddingHorizontal:30,
     }
 })
 export default styles
